4ae209fae796c3f0bc1f58cdc7ab595a5faa1f08
Anele Siwela <113092268+AnelkaCwela@users.noreply.github.com>
2022-09-14 09:06:43 +0200

    Add files via upload

---
+++ Model/IOrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using OurShop.Models.Databinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OurShop.Models
+{
+    public  interface IOrderRepository
+     {
+        IQueryable<Order> Orders { get; }
+        void SaveOrder(Order order);
+    }
+}
